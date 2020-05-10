07ef5f87e0de3d5169eca6040bca040d860b0dae
Roman Rozhkov <roman-rozhkov98@yandex.ru>
2020-05-10 12:43:41 +0300

    changing db_client

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = "933027745:AAHbJN5XJhdJTZP3EuRu-ooTImt7Z3w-lGg"
 ABBYY_KEY = "ZjEyZDRlNjgtYzczZC00ZDRjLWE4ZWQtMzY1ODkzNTM2OTU1OjI1OGJjZDA4ZTFiNTRhMGU5ZGJkODA3Y2FkMTUxNmFi"
-DB_CLIENT = "mongodb://admin:Qa23988798@ds038547.mlab.com:38547/english_trainspoting"
+# DB_CLIENT = "mongodb://admin:Qa23988798@ds038547.mlab.com:38547/english_trainspoting"
+DB_CLIENT = "mongodb://heroku_jq9wgddb:3jtsx2y239887@ds219100.mlab.com:19100/heroku_jq9wgddb"
